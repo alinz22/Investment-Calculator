12e201384f77cc9916eabd9059f8d0b2aa21ae38
Alan Mong <alanmongx@gmail.com>
2024-06-26 03:33:28 -0700

    added results table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 0.06,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, // Ensuring newValue is converted to a number
+      };
+    });
+  }
+
   return (
     <>
-      <Header />;
-      <UserInput />;
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a postive duration</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
   );
 }
